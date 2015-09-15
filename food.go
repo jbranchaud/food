1957fb66b1c4255b67b889abf23b92d3538720c3
jbranchaud <jbranchaud@gmail.com>
2015-09-15 08:43:19 -0500

    Add a bunch of foods and randomly choose one.

--- food.go
+++ food.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "syscall"
 import "os"
+import "math/rand"
 
 func main() {
 	food := RandomFood()
@@ -17,5 +18,36 @@ func main() {
 }
 
 func RandomFood() string {
-	return "lasagna"
+	foods := []string{
+		"lasagna",
+		"pizza",
+		"empanadas",
+		"tacos",
+		"enchiladas",
+		"burritos",
+		"sushi",
+		"fried chicken",
+		"hot wings",
+		"donuts",
+		"cake",
+		"muffins",
+		"pie",
+		"cinnamon rolls",
+		"fries",
+		"fried catfish",
+		"ham",
+		"turkey",
+		"macaroni and cheese",
+		"cheesy potatoes",
+		"cheeseburgers",
+		"cottage cheese",
+		"sausage",
+		"bratwurst",
+		"mozzarella sticks",
+		"cheese bread",
+		"cheese curds",
+		"pasta salad",
+		"beef stew",
+	}
+	return foods[rand.Intn(len(foods))]
 }
